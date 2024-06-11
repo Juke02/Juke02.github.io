63ae407d06350fbb52edbaba8699ed7eace67a7e
Juke02 <danenail182@gmail.com>
2024-06-11 15:28:12 -0500

    Added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,13 @@ function Projects() {
       <div className="desc">
         <h3>A collection of projects I have done.</h3>
       </div>
+      <div className="proj-details">
+        {/* Wrap the project name in an anchor tag */}
+        <a href="https://juke02.github.io/Nontransitive-Dice/" target="_blank" rel="noopener noreferrer">
+          <h3>Nontransitive Dice Calculator</h3>
+        </a>
+        <img src="/dice.png" alt="Dice" className="image" />
+      </div>
     </section>
   );
 }
